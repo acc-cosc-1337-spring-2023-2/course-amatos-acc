afa115d970d984b3072dae59bc05bd6bfcb05517
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-01-25 01:09:27 +0000

    Intro to uni tests, and data types

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,18 @@
+#include "decimals.h" //tell c++ where to find add to double 1 function
+
+#include <iostream>
+
+using std::cout;
+
 int main()
 {
+    double num = 0;
+    double num1 = add_to_double_1(num);
+
+    //we do something with num1
+    num1 = num1 + 1;
+
+    cout<<"Adding to a double: "<<num1<<"\n";
 
     return 0;
 }
