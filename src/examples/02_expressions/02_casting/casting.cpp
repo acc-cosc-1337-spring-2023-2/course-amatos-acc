caa731b924f296a6a41c81531ecc150975d4d296
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-01-27 01:06:49 +0000

    Commit for 01-26-2023

--- src/examples/02_expressions/02_casting/casting.cpp
+++ src/examples/02_expressions/02_casting/casting.cpp
@@ -1,4 +1,4 @@
-
+#include "casting.h"
 /*
 Function convert_int_to_double multiplies years times interest_rate
 
@@ -7,6 +7,11 @@ Function convert_int_to_double multiplies years times interest_rate
 @return the product of years times interest rate
 */
 
+double convert_to_double(int years, double interest_rate)
+{
+    return years * interest_rate;
+}
+
 /*
 Function convert_double_to_int with a double parameter returns the 
 double parameter
@@ -14,6 +19,11 @@ double parameter
 @param double num
 @return the num parameter
 */
+
+int convert_double_to_int(double num)
+{
+    return num;
+}
 
 /*
 Function static_cast_double_int with a double parameter that does
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,21 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "expressions.h"
+#include "casting.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Verify operator_precedence_1", "validating order of operations") {
+	REQUIRE(operator_precedence_1(3, 6, 3) == 5);
+}
+
+TEST_CASE("Verify operator_precedence_2", "validating order of operations with parenthesis") {
+	REQUIRE(operator_precedence_2(3, 6, 3) == 3);
+}
+
+TEST_CASE("Verify convert int to double", "validating int converted to double") {
+	REQUIRE(convert_to_double(10, 0.5) == 5);
+}
+
+
+TEST_CASE("Verify convert double to int", "validating double converted to int") {
+	REQUIRE(convert_double_to_int(10.5) == 10);
 }
