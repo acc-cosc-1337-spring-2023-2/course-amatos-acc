0aed09dd1d2d2e7a2668e33bb8c2ce63cb5114e5
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-03-08 00:58:32 +0000

    classwork march 7th

--- src/examples/08_classes/01_bank/atm.h
+++ src/examples/08_classes/01_bank/atm.h
@@ -1,1 +1,12 @@
 //atm.h
+#include<iostream>
+#include "bank_account.h"
+
+
+void run_menu(BankAccount &account);
+
+
+void display_menu();
+
+
+void handle_menu_option(int option, BankAccount &account);
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,31 @@
+#include<iostream>
+#include "checking_account.h"
+#include "savings_account.h"
+#include "atm.h"
+
+using std::cout;
+using std::cin;
 
 int main()
 {
+	auto choice = 'c';
+	srand(time(NULL));
+
+	SavingsAccount savings;
+	CheckingAccount account;
+
+	cout<<"Checking or Savings: ";
+	cin>>choice;
+
+	if (choice == 'c' || choice == 'C') {
+		cout<<account;
+		cout<<account.get_balance()<<"\n";
+		run_menu(account);
+	} else {
+		cout<<savings;
+		cout<<savings.get_balance()<<"\n";
+		run_menu(savings);
+	}
 
 	return 0;
 }
