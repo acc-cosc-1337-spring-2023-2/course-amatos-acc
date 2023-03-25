caf5c2a7fead35a6a20366d88a8bac6445337beb
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-03-25 01:45:20 +0000

    Homework #6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,31 @@
+#include "tic_tac_toe.h"
 
 int main() 
 {
+	TicTacToe board;
 
+	bool keep_going = true;
+	string player;
+	int position;
+	auto response = 'Y';
+
+	while (keep_going) {
+		cout<<"Enter X or O: ";
+		cin>>player;
+		board.start_game(player);
+		while (!board.game_over()) {
+			cout<<"Enter a position from 1 to 9: ";
+			cin>>position;
+			board.mark_board(position);
+			board.display_board();
+		}
+		cout<<"Game Over\n";
+		cout<<"New game? Y/N: ";
+		cin>>response;
+		if (!(response == 'Y' || response == 'y')) {
+			keep_going = false;
+			cout<<"Exiting...\n\n";
+		}
+	}
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,30 @@
 //h
+#include<string>
+#include<vector>
+#include<iostream>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe {
+
+    string player;
+    vector<string> pegs = {" "," "," "," "," "," "," "," "," "};
+
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+
+    public:
+        bool game_over();
+        void start_game(string first_player);
+        void mark_board(int position);
+        string get_player() const;
+        void display_board() const;
+};
+#endif
