634344c2c0f74e685b684a76fadb366b1f50c399
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-04-02 00:08:57 +0000

    Homework #7

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,30 +1,48 @@
 #include "tic_tac_toe.h"
 
+//return true if player is "X" or "O"
+bool validPlayer(string player) {
+	return player == "X" || player == "O";
+}
+
+//return true is position is 1 thru 9
+bool validPosition(int position) {
+	return position >= 1 && position <= 9;
+}
+
 int main() 
 {
 	TicTacToe board;
 
 	bool keep_going = true;
 	string player;
-	int position;
+	double position;
 	auto response = 'Y';
 
 	while (keep_going) {
 		cout<<"Enter X or O: ";
 		cin>>player;
-		board.start_game(player);
-		while (!board.game_over()) {
-			cout<<"Enter a position from 1 to 9: ";
-			cin>>position;
-			board.mark_board(position);
-			board.display_board();
-		}
-		cout<<"Game Over\n";
-		cout<<"New game? Y/N: ";
-		cin>>response;
-		if (!(response == 'Y' || response == 'y')) {
-			keep_going = false;
-			cout<<"Exiting...\n\n";
+		if (validPlayer(player)) {
+			board.start_game(player);
+			while (!board.game_over()) {
+				cout<<"Enter a position from 1 to 9: ";
+				cin>>position;
+				if (validPosition(position)) {
+					board.mark_board(position);
+					board.display_board();
+				} else {
+					cout<<"Invalid input\n";
+				}
+			}
+			cout<<"Game Over! The winner is: "<<board.get_winner()<<"\n\n";
+			cout<<"New game? Y/N: ";
+			cin>>response;
+			if (!(response == 'Y' || response == 'y')) {
+				keep_going = false;
+				cout<<"Exiting...\n\n";
+			}
+		} else {
+			cout<<"Invalid input!\n";
 		}
 	}
 	return 0;
