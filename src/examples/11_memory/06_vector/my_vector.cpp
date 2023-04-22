2955d443616ae560a824f9ef53a296caae6aa26a
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-04-22 17:07:36 +0000

    classwork april 20th

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,19 @@
 #include "my_vector.h"
 
-//
+Vector::Vector(int s) : capacity{0}, elements{new int[s]} {
+    cout<<"created new memory at"<<elements<<"\n";
+}
 
+Vector::~Vector() {
+    cout<<"delete the memory at"<<elements<<"\n";
+    delete[] elements;
+}
+
+//Not a class function; these are free functions
+void use_vector() {
+    //do not create dynamic memory for v; use a stack variable
+    //Vector* v = new Vector(3);
+    //delete v;
+
+    Vector v(3);
+}
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,26 @@
-//
+#include<iostream>
+
+using std::cout;
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector {
+    public:
+        Vector(int s);
+        //Rule 1 of 3 (3 rules of legacy c++)
+        Vector(const Vector& v); //copy constructor
+        Vector& operator=(const Vector& v); //Copy assignment Rule 2 of 3 in legacy c++
+        int Size() const{return size;}
+        int Capacity() const{return capacity;}
+        ~Vector(); //rule 3 of 3
+    private:
+        int* elements; //holds the dynamic list on the heap
+        int size; //current size of the list; doubles as the current_index
+        int capacity; //total available slots for elements
+
+};
+
+#endif
+
+void use_vector();
